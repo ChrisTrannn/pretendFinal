4a139e69a9b0123a8e76765b3965c9a0e8b78436
Chris Tran <christran1773@gmail.com>
2023-05-21 13:41:34 -0700

    finished beach page

--- src/components/beach/Beach.js
+++ src/components/beach/Beach.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Route, Link, Routes, useNavigate} from 'react-router-dom';
 import './Beach.css';
 import beachGIF from './beach.gif';
+import backButton from '../../assets/backButton.png'
+import pcCharacter from '../../assets/pcCharacter.png'
 
 const Beach = () => {
     const navigate = useNavigate();
@@ -11,67 +13,75 @@ const Beach = () => {
     };
 
     return (
-        <div className='beachBody'>
-            <button onClick={() => handleClick('/home')}>Back</button>	
+        <div>
 
-            <div className="heading"> Welcome to the Beach </div>
-
-            <img className='picture' src={beachGIF}></img>
-
-            <div className="description"> 
-                <p> 
-                    Playing in the beach can help kids learn about various 
-                    science concepts such as erosion, gravity, and the 
-                    properties of matter such as weight, texture, and density. 
-                </p>
+            <div className='top'>
+                <img src={backButton} onClick={() => handleClick('/home')} className='backButton'></img>
+                <div className='topTitle'>Welcome to the Beach</div>
+                <img className='topCharacter' src={pcCharacter}></img>
             </div>
 
-            <div className="learning">
-                <p> Learning Objectives </p>
-            </div>
-
-            <div className="row">
-                <div className="column">
-                    <div className="card">
-                        <h3> Museum Activities </h3>
-                        <p> Click to learn about a museum activity </p>
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="column">
-                        <div className="card">
-                            <h3> At Home Activities </h3>
-                            <p> 
-                                Click to learn about at home activities that 
-                                involve buoyancy, water tension, and more 
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="column">
-                        <div className="card">
-                            <h3> Science Concepts </h3>
-                            <p> Click to learn more about how a boat floats on water </p>
-                        </div>
-                    </div>
-                        
-                    <div className="column">
-                        <div className="card">
-                            <h3> Fun Facts! </h3>
-                            <p> Click to learn about fun facts! </p>
-                        </div>
-                    </div>
-                </div>
-                
-                <div className="learned">
-                    <h3> What Your Child Learned </h3>
-                    <p> What learned </p>
+            <div className='imageDesc'>
+                <img src={beachGIF} className='gif'></img>
+                <div className='imageDescription'>
+                    Playing in the beach can help kids learn about
+                    various science concepts such as erosion, gravity, 
+                    and the properties of matter such as weight, texture, 
+                    and density.
                 </div>
             </div>
 
+            <div className='learningObjectives'>
+                <div className='learningObjectivesTitle'>
+                    Learning Objectives
+                </div>
+                <div className='objectiveCards'>
+                    <div className='objectiveCard'>
+                        <div className='objectiveTitle'>
+                            Museum Activities
+                        </div>
+                        <div className='objectiveDescription'>
+                            Click to learn about a museum activity
+                        </div>
+                    </div>
+                    <div className='objectiveCard'>
+                        <div className='objectiveTitle'>
+                            At Home Activities
+                        </div>
+                        <div className='objectiveDescription'>
+                            Click to learn about at home activities that involve
+                            buoyancy, water tension, and more
+                        </div>
+                    </div>
+                </div>
+                <div className='objectiveCards objectiveCards2'>
+                    <div className='objectiveCard'>
+                        <div className='objectiveTitle'>
+                            Science Concepts
+                        </div>
+                        <div className='objectiveDescription'>
+                            Click to learn more about how a boat floats on water
+                        </div>
+                    </div>
+                    <div className='objectiveCard'>
+                        <div className='objectiveTitle'>
+                            Fun Facts
+                        </div>
+                        <div className='objectiveDescription'>
+                            Click to learn about fun facts!
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='childLearn'>
+                <div className='childLearnTitle'>
+                    What Your Child Learned
+                </div>
+                <div className='childLearnDescription'>
+                    Your child learned about buoyancy, water tension, and more!
+                </div>
+            </div>
         </div>
     )
 }
